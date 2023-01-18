a16348360d9e147255c71fdabfcbf236aad787be
rupeshIlogsolution <rupesh.kumar@ilogsolution.com>
2023-01-18 23:34:10 +0530

    UI Start

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,18 @@ const Home = () => {
     return (
         <>
             <h1>Home</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY3eMsvXGJXNJF4woNGXa4QHvTwcLcdgKIvP3VWXiydSawoH7nSmj4bZw47N8SfoHUulQ&usqp=CAU' alt='kjb'/>
+                </div>
+                <div className="text-wrapper item">
+                <span>
+                    
+                </span>
+                </div>
+                <div className="img-wrapper item">
+                </div>
+            </div>
         </>
     )
 }
