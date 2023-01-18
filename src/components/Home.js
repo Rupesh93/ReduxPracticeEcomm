c48b0a013dea21c3326b8c12f7a8a071a8fa122c
rupeshIlogsolution <rupesh.kumar@ilogsolution.com>
2023-01-18 23:45:19 +0530

    UI Start

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,16 @@ const Home = () => {
             <h1>Home</h1>
             <div className="cart-wrapper">
                 <div className="img-wrapper item">
-                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY3eMsvXGJXNJF4woNGXa4QHvTwcLcdgKIvP3VWXiydSawoH7nSmj4bZw47N8SfoHUulQ&usqp=CAU' alt='kjb'/>
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY3eMsvXGJXNJF4woNGXa4QHvTwcLcdgKIvP3VWXiydSawoH7nSmj4bZw47N8SfoHUulQ&usqp=CAU' alt='kknjb' />
                 </div>
                 <div className="text-wrapper item">
-                <span>
-                    
-                </span>
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>Price: $1000.00</span>
                 </div>
-                <div className="img-wrapper item">
+                <div className="btn-wrapper item">
+                <button>Add to Cart</button>
                 </div>
             </div>
         </>
